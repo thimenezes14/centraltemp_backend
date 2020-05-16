e000ac68e537a3c242d1917c376d372fd29c097f
Thiago Menezes <thi.menezes14@gmail.com>
2020-05-16 20:22:42 -0300

    Criação de limite de requisições por janela de tempo por usuário

--- api/config/server.js
+++ api/config/server.js
@@ -3,11 +3,18 @@ const app = express();
 const morgan = require('morgan');
 const routes = require('../routes');
 const path = require('path');
+const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+    windowMs: process.env.WINDOW_MINUTES * 60 * 1000,
+    max: process.env.MAX_REQUESTS_PER_WINDOW,
+    message: "Limite de requisições atingido. Por favor, tente novamente em 10 minutos."
+});
+
+app.use(limiter);
 app.use('/imagens', express.static(path.resolve(__dirname, '..', 'avatar')));
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('./database/mongoose');
 require('./database/sequelize');
 
 const app = require('./api/config/server');
-const environment = process.env.NODE_ENV || 'development';
-console.log(environment);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Aplicação iniciada na porta ${process.env.SERVER_PORT}!`);
